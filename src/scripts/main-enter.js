9065989217286cca8b9c67ba9692c03a838df661
Matthew Caldwell <caldwellysr@gmail.com>
2016-04-16 17:05:31 -0400

    scaleCanvas

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,18 +1,20 @@
 "use strict";
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
-	var scores = game.entities.get(0,"scores");
-	console.log(game.arguments);
-	if (game.arguments["scores"]) {
-		scores.round1 = game.arguments["scores"].round1;
-		scores.round2 = game.arguments["scores"].round2;
-		scores.round3 = game.arguments["scores"].round3;
-	} else {
-		scores.round1 = 0;
-		scores.round2 = 0;
-		scores.round3 = 0;
-	}
 
-	game.entities.set(0,"round",game.arguments["round"] || 0);
-	console.log(scores);
+    game.scaleCanvasToFitRectangle(1280,960);
+    var scores = game.entities.get(0,"scores");
+    console.log(game.arguments);
+    if (game.arguments["scores"]) {
+        scores.round1 = game.arguments["scores"].round1;
+        scores.round2 = game.arguments["scores"].round2;
+        scores.round3 = game.arguments["scores"].round3;
+    } else {
+        scores.round1 = 0;
+        scores.round2 = 0;
+        scores.round3 = 0;
+    }
+
+    game.entities.set(0,"round",game.arguments["round"] || 0);
+    console.log(scores);
 };
