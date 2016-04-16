00d59864c11ba481aed6caae947654ce27672f13
Matthew Caldwell <caldwellysr@gmail.com>
2016-04-15 22:50:32 -0400

    movement and rotation very rough

--- src/systems/simulation/sample-simulation-system.js
+++ src/systems/simulation/sample-simulation-system.js
@@ -1,6 +1,27 @@
 "use strict";
 
+var velocity, rotation;
+
+function getVelocity(game, angle, speed) {
+    return {
+        "x": speed * Math.cos(angle),
+        "y": speed * Math.sin(angle)
+    };
+}
+
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
-  ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
-  }, "playerController2d");
+    ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
+        rotation = game.entities.get(entity, "rotation");
+        if (game.inputs.button("go")) {
+            velocity = getVelocity(game, rotation.angle, game.entities.get(entity, "speed"));
+            game.entities.set(entity, "velocity", velocity);
+        } else {
+            game.entities.set(entity, "velocity", { "x": 0, "y": 0 });
+        }
+        if (game.inputs.button("clockwise") && !game.inputs.button("counter_clockwise")) {
+            rotation.angle += game.entities.get(entity, "angle_mod");
+        } else if (game.inputs.button("counter_clockwise") && !game.inputs.button("clockwise")) {
+            rotation.angle -= game.entities.get(entity, "angle_mod");
+        }
+    }, "player");
 };
