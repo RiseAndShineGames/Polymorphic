7f7aa40c6fd82ba3bc54dbe27a17916ebcc0e0ce
Matthew Caldwell <caldwellysr@gmail.com>
2016-04-15 23:58:39 -0400

    add player animation

--- src/systems/simulation/sample-simulation-system.js
+++ src/systems/simulation/sample-simulation-system.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var velocity, rotation;
+var velocity, rotation, animation;
 
 function getVelocity(game, angle, speed) {
     return {
@@ -12,11 +12,14 @@ function getVelocity(game, angle, speed) {
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
     ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
         rotation = game.entities.get(entity, "rotation");
+        animation = game.entities.get(entity, "animation");
         if (game.inputs.button("go")) {
             velocity = getVelocity(game, rotation.angle, game.entities.get(entity, "speed"));
             game.entities.set(entity, "velocity", velocity);
+            animation.speed = 1;
         } else {
             game.entities.set(entity, "velocity", { "x": 0, "y": 0 });
+            animation.speed = 0;
         }
         if (game.inputs.button("clockwise") && !game.inputs.button("counter_clockwise")) {
             rotation.angle += game.entities.get(entity, "angle_mod");
