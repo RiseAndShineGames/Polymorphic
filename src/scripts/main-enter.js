8bc4399aad3cce9b2f573d9172c8f1f675b56639
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-04-15 23:43:26 -0400

    advance scenes

--- src/scripts/main-enter.js
+++ src/scripts/main-enter.js
@@ -1,4 +1,17 @@
 "use strict";
 
 module.exports = function(game) { // eslint-disable-line no-unused-vars
+	var scores = game.entities.get(0,"scores");
+	if (game.arguments["rounds"]) {
+		scores.round1 = game.arguments["rounds"].round1;
+		scores.round2 = game.arguments["rounds"].round2;
+		scores.round3 = game.arguments["rounds"].round3;
+	} else {
+		scores.round1 = 0;
+		scores.round2 = 0;
+		scores.round3 = 0;
+	}
+
+	game.entities.set(0,"round",game.arguments["round"] || 0);
+	console.log(scores);
 };
