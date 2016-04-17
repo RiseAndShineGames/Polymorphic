75d137d179e64a225c85d2585579bc7c1faa4918
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-04-17 13:15:03 -0400

    add spotlight q

--- src/systems/renderer/crop-game.js
+++ src/systems/renderer/crop-game.js
@@ -9,28 +9,32 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 		var cameraSize = game.entities.get(0, "size");
 
 		context.fillStyle = "black";
+		//top
 		context.fillRect(
 			cameraPosition.x,
 			cameraPosition.y,
 			cameraSize.width,
-			viewportPosition.y - cameraPosition.y
+			viewportPosition.y - cameraPosition.y + 2
 		);
+		//right
 		context.fillRect(
-			viewportPosition.x + viewportSize.width,
+			viewportPosition.x + viewportSize.width - 2,
 			cameraPosition.y,
 			(cameraPosition.x + cameraSize.width) - (viewportPosition.x + viewportSize.width),
 			cameraSize.height
 		);
+		//bottom
 		context.fillRect(
 			cameraPosition.x,
-			viewportPosition.y + viewportSize.height,
+			viewportPosition.y + viewportSize.height - 2,
 			cameraSize.width,
 			(cameraPosition.y + cameraSize.height) - (viewportPosition.y + viewportSize.height)
 		);
+		//left
 		context.fillRect(
 			cameraPosition.x,
 			cameraPosition.y,
-			viewportPosition.x - cameraPosition.x,
+			viewportPosition.x - cameraPosition.x + 2,
 			cameraSize.height
 		);
 
