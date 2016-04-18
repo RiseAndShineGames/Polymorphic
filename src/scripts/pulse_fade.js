5034c512f2c837007b93db90d4c837c02a894c9b
Anthony Quisenberry <anthony.quisenberry01@gmail.com>
2016-04-18 15:40:49 -0400

    smoother pulse

--- src/scripts/pulse_fade.js
+++ src/scripts/pulse_fade.js
@@ -5,7 +5,7 @@ var timers,image, ticks = 0.00;
 module.exports = function(entity, game) {
     ticks += .03;
     image = game.entities.get(entity,"image");
-    image.alpha = Math.abs(Math.sin(ticks)) + .1;
+    image.alpha = 0.85 * (Math.sin(ticks) + 1) / 2 + .15;
 
     timers = game.entities.get(entity, "timers");
     timers.pulse.time = 0;
